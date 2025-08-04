4d9db0c8845b7f55c54b44d5bedf92db0ea139a6
Artem Prokop <artem13.prokop@gmail.com>
2025-08-04 16:09:03 +0200

    fix: memes

--- src/controllers/meme.ts
+++ src/controllers/meme.ts
@@ -1,40 +1,38 @@
 import { Composer } from 'grammy';
-
 import type { CustomContext } from '../types/context.js';
 
 export const memeController = new Composer<CustomContext>();
 
 memeController.command('meme', async ctx => {
-  const [, ...parts] = ctx.message?.text?.split(' ') || [];
-  const fullText = parts.join(' ');
-
-  if (!fullText.includes(' / ')) {
+  if (!ctx.match || !ctx.from) {
     await ctx.text('meme.usage');
     return;
   }
 
-  const [topText, bottomText] = fullText.split(' / ').map(text => text.trim());
-
-  if (!topText || !bottomText) {
+  const parts = ctx.match.trim().split('/');
+  if (parts.length !== 2) {
     await ctx.text('meme.usage');
     return;
   }
+
+  const topText = parts[0].trim().replace(/\s+/g, '_');
+  const bottomText = parts[1].trim().replace(/\s+/g, '_');
 
   const baseUrl = 'https://memecomplete.com/share/images/custom';
-  const memeUrl = `${baseUrl}/${encodeURIComponent(topText)}/${encodeURIComponent(bottomText)}.jpg`;
+  const backgroundParam =
+    'https%3A%2F%2Fexternal-content.duckduckgo.com%2Fiu%2F%3Fu%3Dhttps%253A%252F%252Fwww.meme-arsenal.com%252Fmemes%252F753d2cb2e64bb0ff7144f2b1b203132d.jpg%26f%3D1%26nofb%3D1%26ipt%3D410106a1840acb84707a7bf33472b424b80353f739a38ae1cb6abde41067b253';
+  const token = '0czc5w59hy830pj22koi';
 
-  const message = `<a href="${memeUrl}">⁠</a>${topText} / ${bottomText}`;
+  const memeUrl = `${baseUrl}/${encodeURIComponent(topText)}~q/${encodeURIComponent(bottomText)}?format=jpg&background=${backgroundParam}&token=${token}`;
 
   try {
-    await ctx.reply(message, { parse_mode: 'HTML' });
-
+    await ctx.replyWithPhoto(memeUrl);
     try {
       await ctx.deleteMessage();
     } catch {
-      void 0;
+      //ignore
     }
   } catch (error) {
     console.error(error);
-    await ctx.text('meme.error');
   }
 });
